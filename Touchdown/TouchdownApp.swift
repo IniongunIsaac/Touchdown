4c4034e2d3923712568cf06fab0c5ea6bef61d88
Isaac Iniongun <iniongunisaac@gmail.com>
2023-03-25 22:56:21 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Isaac Iniongun on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
